0e0bf56227a4876bfc6fa69a691ab0aea3029b5c
Samantha-Ramirez <samantha.p.ramirez.y@gmail.com>
2025-06-21 13:57:52 -0400

    Casos de prueba Parte 1, 3

--- CasosDePrueba.pl
+++ CasosDePrueba.pl
@@ -12,19 +12,20 @@ barrel("C", 3, 0).
 /* 
     Parte 2: Existe solución 
 */
+% Base de conocimientos
+initialBarrels(["A", "B", "C"], [10, 7, 3], [1, 3, 0]).
+
 ?- iSolution("A", 1, 2).
 true.
 ?- iSolution("C", 3, 4).
 false.
 
-% Base de conocimientos
-barrel("A", 10, 1).
-barrel("B", 7, 3).
-barrel("C", 3, 0).
-
 /* 
     Parte 3: Añadir cerveza 
 */
+% Base de conocimientos
+initialBarrels(["A", "B", "C"], [20, 7, 4], [15, 0, 3]).
+
 ?- addBeer("A", 5, Transfer).
 Transfer = 0.
 
@@ -32,5 +33,25 @@ Transfer = 0.
 Transfer = 4.
 
 % Base de conocimientos
-barrel("A", 20, 15).
-barrel("C", 4, 3).
+initialBarrels(["A", "B", "C"], [9, 7, 4], [0, 0, 3]).
+
+?- addBeer("A", 10, Transfer).
+Transfer = 1. % (9, 9)
+
+% Base de conocimientos
+initialBarrels(["A", "B", "C"], [999, 7, 4], [998, 0, 3]).
+
+?- addBeer("A", 1000, Transfer).
+Transfer = 999. % (999,999)
+
+% Base de conocimientos
+initialBarrels(["A", "B", "C"], [1000, 7, 4], [1000, 0, 3]).
+
+?- addBeer("A", 1000, Transfer).
+Transfer = 1000. % (1000,1000)
+
+% Base de conocimientos
+initialBarrels(["A", "B", "C"], [0, 7, 4], [0, 0, 3]).
+
+?- addBeer("A", 1000, Transfer).
+Transfer = 1000. % (0,0)
